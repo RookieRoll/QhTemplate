37006f4ab8799b62dea350088e4262ca202ea751
RookieRoll <2807645688@qq.com>
2018-01-15 10:41:22 +0800

    finish menus

---
+++ QhTemplate.ApplicationService/PyScripts/Briefing.py
@@ -0,0 +1,9 @@
+class Briefing():
+    CompanyName=""
+    Link=""
+    OptionLink=""
+
+    def __init__(self,companyName,link,optionLink):
+        self.CompanyName=companyName
+        self.Link=link
+        self.OptionLink=optionLink
---
+++ QhTemplate.ApplicationService/PyScripts/BriefingContent.py
@@ -0,0 +1,17 @@
+class BriefingContent():
+    # 公司名称
+    CompanyName=""
+    # 举办学校
+    School=""
+    # 举办教学楼
+    Held=""
+    # 宣讲时间
+    StartTime=""
+    # 发布时间
+    PublishTime=""
+    # 宣讲正文
+    Body=""
+    # 职位列表
+    JobList=[]
+    # 操作链接
+    OpthonList=""
