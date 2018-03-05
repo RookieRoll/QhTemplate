b107ad2b0f4548081283822bf4c913440d7a2280
RookieRoll <2807645688@qq.com>
2018-03-05 21:50:27 +0800

    add articles

---
+++ QhTemplate.ApplicationService/NewsArticles/ArticlesApplicationService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using QhTemplate.ApplicationCore.News;
+using QhTemplate.MysqlEntityFrameWorkCore.Models;
+
+namespace QhTemplate.ApplicationService.NewsArticles
+{
+    public class ArticlesApplicationService : IArticlesApplicationService
+    {
+        private readonly ArticleManagers _article;
+
+        public ArticlesApplicationService(ArticleManagers article)
+        {
+            _article = article;
+        }
+
+        public IQueryable<NewArticle> Finds()
+        {
+            return _article.Finds().AsQueryable();
+        }
+
+        public IQueryable<NewArticle> Finds(Func<NewArticle, bool> func)
+        {
+            return _article.Finds(func).AsQueryable();
+        }
+
+        public NewArticle FirstOrDefault(Func<NewArticle, bool> func)
+        {
+            return _article.FirstOrDefault(func);
+        }
+
+        public NewArticle First(Func<NewArticle, bool> func)
+        {
+            return _article.First(func);
+        }
+
+        public NewArticle Find(int id)
+        {
+            return _article.Find(id);
+        }
+
+        public void Create(string title, string content)
+        {
+            var article = NewArticle.Create(title, content);
+            _article.Create(article);
+        }
+
+        public void Remove(int id)
+        {
+            _article.Deleted(id);
+        }
+
+        public void Update(NewArticle area)
+        {
+           _article.Update(area);
+        }
+    }
+}
---
+++ QhTemplate.ApplicationService/NewsArticles/IArticlesApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using QhTemplate.MysqlEntityFrameWorkCore.Models;
+
+namespace QhTemplate.ApplicationService.NewsArticles
+{
+    public interface IArticlesApplicationService
+    {
+        IQueryable<NewArticle> Finds();
+        IQueryable<NewArticle> Finds(Func<NewArticle, bool> func);
+        NewArticle FirstOrDefault(Func<NewArticle, bool> func);
+        NewArticle First(Func<NewArticle, bool> func);
+        NewArticle Find(int id);
+        void Create(string title, string content);
+        void Remove(int id);
+        void Update(NewArticle area);
+    }
+
+}
