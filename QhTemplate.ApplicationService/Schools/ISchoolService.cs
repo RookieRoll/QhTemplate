16238308b3dadb2616c8b41c8a37283e7c994fec
RookieRoll <2807645688@qq.com>
2018-03-18 21:30:00 +0800

    add some file

---
+++ QhTemplate.ApplicationService/Schools/ISchoolService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using QhTemplate.MysqlEntityFrameWorkCore.Models;
+
+namespace QhTemplate.ApplicationService.Schools
+{
+    public interface ISchoolService
+    {
+        IQueryable<SchoolArea> Finds();
+        IQueryable<SchoolArea> Finds(Func<SchoolArea, bool> func);
+        SchoolArea FirstOrDefault(Func<SchoolArea, bool> func);
+        SchoolArea First(Func<SchoolArea, bool> func);
+        SchoolArea Find(int id);
+        void Create(string name, string code,string path,string address);
+        void Remove(int id);
+        void Update(SchoolArea area);
+    }
+}
